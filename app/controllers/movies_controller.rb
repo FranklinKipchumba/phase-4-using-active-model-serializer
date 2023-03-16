58fa1096488396e53399fdfb0c7c85bef91ea174
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-03-16 12:11:09 +0300

    Completed assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,13 +2,23 @@ class MoviesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    movies = Movie.all
-    render json: movies
+    movie = Movie.all
+    render json: movie.to_json(only: [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
   end
 
   def show
     movie = Movie.find(params[:id])
     render json: movie
+  end
+
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
   end
 
   private
